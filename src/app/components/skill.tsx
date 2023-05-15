6e30188f5cbcfa77813b5999f44cc05e7425c0bd
taewaponb <taewapon.b@gmail.com>
2023-05-16 03:09:07 +0700

    Add template, component, mock data and update on page.tsx

---
+++ src/app/components/skill.tsx
@@ -0,0 +1,164 @@
+export const Skill = (props: any) => {
+  return (
+    <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-5 lg:text-left">
+      <a className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+      </a>
+
+      <a className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+      </a>
+
+      <a className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+      </a>
+
+      <a className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+      </a>
+
+      <a className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+        <h2 className={`mb-3 text-xl font-semibold`}>
+          Skill{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-xs opacity-50`}>Description</p>
+        <br />
+      </a>
+    </div>
+  );
+};
