4884df141a7f3cebf14a2c61bf3630d65dc55d06
taewaponb <taewapon.b@gmail.com>
2023-05-17 02:27:56 +0700

    Update style on skill component

--- src/app/components/skill.tsx
+++ src/app/components/skill.tsx
@@ -3,15 +3,17 @@
 import Image from "next/image";
 import { Key, useEffect, useState } from "react";
 
+const commonStyle = `transition-transform motion-reduce:transform-none ease-in-out duration-500`;
+
 export const Skill = (props: any) => {
   const [skillData, setSkillData] = useState<any>([]);
   const skillList = [
     [
-      { id: 1111, level: 0 },
-      { id: 2678, level: 0 },
-      { id: 8485, level: 0 },
-      { id: 964, level: 0 },
-      { id: 6432, level: 0 },
+      { id: 1111, level: 10 },
+      { id: 2678, level: 20 },
+      { id: 8485, level: 4 },
+      { id: 964, level: 4 },
+      { id: 6432, level: 20 },
     ],
     [
       { id: 5653, level: 0 },
@@ -40,7 +42,7 @@ export const Skill = (props: any) => {
 
   const checkIsContainSkill = (skill: number) => skill !== 0;
   const textSkillLevel = (level: number) =>
-    level === 0 ? "" : level === 20 ? "MAX" : level;
+    level === 0 ? "" : level === 20 ? "max" : level;
 
   const updateSkillLevel = (skillId: number, value: number) => {
     const newLevelValue = (level: number) => {
@@ -60,9 +62,7 @@ export const Skill = (props: any) => {
   };
 
   return (
-    <div
-      className={`mb-32 grid text-center lg:mb-0 lg:grid-cols-5 lg:text-center`}
-    >
+    <div className={`mb-24 grid text-center lg:mb-0 lg:grid-cols-5`}>
       {skillData.map((data: { id: number; level: number }[]) =>
         data.map(
           (
@@ -70,19 +70,16 @@ export const Skill = (props: any) => {
             index: Key | null | undefined
           ) =>
             checkIsContainSkill(skill.id) ? (
-              <a
-                key={index}
-                className="group relative rounded-lg border border-transparent px-1 py-0.5"
-              >
+              <span key={index} className="group relative">
                 <h2 className={`text-3xl font-semibold`}>
                   <span
-                    className="inline-block transition-transform translate-x-8 hover:scale-125 hover:cursor-pointer group-hover:translate-x-0 motion-reduce:transform-none ease-in-out duration-500 text-green-200"
+                    className={`${commonStyle} inline-block translate-x-8 group-hover:translate-x-0 hover:scale-125 hover:cursor-pointer text-green-200`}
                     onClick={() => updateSkillLevel(skill.id, 1)}
                   >
                     +
                   </span>{" "}
                   <Image
-                    className="inline-block relative z-10 transition-transform group-hover:scale-110 ease-in-out duration-500"
+                    className={`${commonStyle} inline-block relative z-10 group-hover:scale-110`}
                     src={`/skill/${skill.id}.png`}
                     alt="skill"
                     width={40}
@@ -90,17 +87,19 @@ export const Skill = (props: any) => {
                     priority
                     draggable={false}
                   />
-                  <span className="absolute z-20 transition-transform group-hover:scale-110 -right-2.5 text-sm -translate-x-10 translate-y-5 drop-shadow-[1px_1px_3px_#FF0000] duration-250">
+                  <span
+                    className={`${commonStyle} inline-block absolute z-20  -right-3 text-sm -translate-x-10 translate-y-5 group-hover:scale-110 drop-shadow-[1px_1px_3px_#FF0000]`}
+                  >
                     {textSkillLevel(skill.level)}
                   </span>{" "}
                   <span
-                    className="inline-block transition-transform -translate-x-8 hover:scale-125 hover:cursor-pointer group-hover:translate-x-0 motion-reduce:transform-none ease-in-out duration-500 text-red-200"
+                    className={`${commonStyle} inline-block -translate-x-8 group-hover:translate-x-0 hover:scale-125 hover:cursor-pointer text-red-200`}
                     onClick={() => updateSkillLevel(skill.id, -1)}
                   >
                     -
                   </span>
                 </h2>
-              </a>
+              </span>
             ) : (
               <span />
             )
