fec9dab0f199fb15d799386beb5c9cc2289f3832
taewaponb <taewapon.b@gmail.com>
2023-05-17 01:43:21 +0700

    Change app name and fix bug on skill component

--- src/app/components/skill.tsx
+++ src/app/components/skill.tsx
@@ -7,56 +7,53 @@ export const Skill = (props: any) => {
   const [skillData, setSkillData] = useState<any>([]);
   const skillList = [
     [
-      { id: 1111, level: 20 },
-      { id: 2678, level: 20 },
-      { id: 8485, level: 20 },
-      { id: 964, level: 4 },
+      { id: 1111, level: 0 },
+      { id: 2678, level: 0 },
+      { id: 8485, level: 0 },
+      { id: 964, level: 0 },
       { id: 6432, level: 0 },
     ],
     [
-      { id: 5653, level: 4 },
-      { id: 3964, level: 20 },
-      { id: 9852, level: 20 },
-      { id: 3721, level: 20 },
-      { id: 1029, level: 20 },
-    ],
-    [
-      { id: 0, level: 11 },
-      { id: 1129, level: 20 },
-      { id: 7661, level: 20 },
-      { id: 690, level: 20 },
-      { id: 6858, level: 20 },
+      { id: 5653, level: 0 },
+      { id: 3964, level: 0 },
+      { id: 9852, level: 0 },
+      { id: 3721, level: 0 },
+      { id: 1029, level: 0 },
     ],
     [
       { id: 0, level: 0 },
-      { id: 2615, level: 4 },
-      { id: 3494, level: 20 },
+      { id: 1129, level: 0 },
+      { id: 7661, level: 0 },
+      { id: 690, level: 0 },
+      { id: 6858, level: 0 },
+    ],
+    [
+      { id: 0, level: 0 },
+      { id: 2615, level: 0 },
+      { id: 3494, level: 0 },
       { id: 7773, level: 0 },
       { id: 6451, level: 0 },
     ],
   ];
 
-  useEffect(() => {
-    setSkillData(skillList);
-  }, []);
+  useEffect(() => setSkillData(skillList), []);
 
   const checkIsContainSkill = (skill: number) => skill !== 0;
-  const displaySkillLevel = (level: number) =>
+  const textSkillLevel = (level: number) =>
     level === 0 ? "" : level === 20 ? "MAX" : level;
 
-  const onUpgrade = (skillId: number) => {
-    const newSkillData = skillData.map((data: any) =>
-      data.map((skill: any) =>
-        skill.id === skillId ? { ...skill, level: skill.level + 1 } : skill
-      )
-    );
-    setSkillData(newSkillData);
-  };
+  const updateSkillLevel = (skillId: number, value: number) => {
+    const newLevelValue = (level: number) => {
+      if (level + value === 21) return 20;
+      else if (level + value === -1) return 0;
+      else return level + value;
+    };
 
-  const onDowngrade = (skillId: number) => {
     const newSkillData = skillData.map((data: any) =>
       data.map((skill: any) =>
-        skill.id === skillId ? { ...skill, level: skill.level - 1 } : skill
+        skill.id === skillId
+          ? { ...skill, level: newLevelValue(skill.level) }
+          : skill
       )
     );
     setSkillData(newSkillData);
@@ -80,7 +77,7 @@ export const Skill = (props: any) => {
                 <h2 className={`text-3xl font-semibold`}>
                   <span
                     className="inline-block transition-transform translate-x-8 hover:scale-125 hover:cursor-pointer group-hover:translate-x-0 motion-reduce:transform-none ease-in-out duration-500 text-green-200"
-                    onClick={() => onUpgrade(skill.id)}
+                    onClick={() => updateSkillLevel(skill.id, 1)}
                   >
                     +
                   </span>{" "}
@@ -94,11 +91,11 @@ export const Skill = (props: any) => {
                     draggable={false}
                   />
                   <span className="absolute z-20 transition-transform group-hover:scale-110 -right-2.5 text-sm -translate-x-10 translate-y-5 drop-shadow-[1px_1px_3px_#FF0000] duration-250">
-                    {displaySkillLevel(skill.level)}
+                    {textSkillLevel(skill.level)}
                   </span>{" "}
                   <span
                     className="inline-block transition-transform -translate-x-8 hover:scale-125 hover:cursor-pointer group-hover:translate-x-0 motion-reduce:transform-none ease-in-out duration-500 text-red-200"
-                    onClick={() => onDowngrade(skill.id)}
+                    onClick={() => updateSkillLevel(skill.id, -1)}
                   >
                     -
                   </span>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Flyff Universe Skill Simulator | By Taewapon B.",
+  title: "Flyff Universe Skill Planner | By Taewapon B.",
   description: "A skill simulator for Flyff Universe.",
 };
 
