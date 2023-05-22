1dc31b01807e579682d8824460368c52d67584ae
taewaponb <taewapon.b@gmail.com>
2023-05-23 00:22:32 +0700

    Update skill layout for future class

--- src/app/components/skill.tsx
+++ src/app/components/skill.tsx
@@ -52,45 +52,53 @@ export const Skill = (props: any) => {
     <div className="grid text-center">
       <p className={`p-2 opacity-50`}>{props.class}</p>
       <div
-        className={`grid text-center grid-cols-5 mb-12 lg:mb-0  hover:cursor-pointer`}
+        className={`grid text-center grid-cols-5 mb-12 lg:mb-0 hover:cursor-pointer`}
       >
-        {skillData.map((data: { id: number; level: number }[]) =>
-          data.map((skill: { id: number; level: number }, index: Key) =>
-            checkIsContainSkill(skill.id) ? (
-              <span key={index} className="group relative">
-                <h2 className={`text-xs lg:text-3xl font-semibold`}>
-                  <span
-                    className={`${commonStyle} inline-block translate-x-8 group-hover:translate-x-0 hover:scale-125 text-green-400`}
-                    onClick={() => updateSkillLevel(skill.id, 1)}
-                  >
-                    +
-                  </span>{" "}
-                  <Image
-                    className={`${commonStyle} inline-block relative z-10 group-hover:scale-110`}
-                    src={`/skill/${skill.id}.png`}
-                    alt="skill"
-                    width={40}
-                    height={40}
-                    priority
-                    draggable={false}
-                    onClick={() => updateSkillLevel(skill.id, 20)}
-                  />
-                  <span
-                    className={`${commonStyle} inline-block absolute z-20 -right-6 lg:-right-3 text-sm -translate-x-10 translate-y-5 group-hover:scale-110 drop-shadow-[1px_1px_3px_#FF0000]`}
-                  >
-                    {textSkillLevel(skill.level)}
-                  </span>{" "}
-                  <span
-                    className={`${commonStyle} inline-block -translate-x-8 group-hover:translate-x-0 hover:scale-125 text-red-400`}
-                    onClick={() => updateSkillLevel(skill.id, -1)}
-                  >
-                    -
-                  </span>
-                </h2>
-              </span>
-            ) : (
-              <span key={index} />
-            )
+        {skillData.map(
+          (data: { id: number; level: number }[], skillIndex: number) => (
+            <div
+              key={skillIndex}
+              className={`grid text-center grid-rows-4 mb-12 lg:mb-0 hover:cursor-pointer`}
+            >
+              {data.map(
+                (skill: { id: number; level: number }, dataIndex: number) =>
+                  checkIsContainSkill(skill.id) ? (
+                    <span key={dataIndex} className="group relative">
+                      <h2 className={`text-xs lg:text-3xl font-semibold`}>
+                        <span
+                          className={`${commonStyle} inline-block translate-x-8 group-hover:translate-x-0 hover:scale-125 text-green-400`}
+                          onClick={() => updateSkillLevel(skill.id, 1)}
+                        >
+                          +
+                        </span>{" "}
+                        <Image
+                          className={`${commonStyle} inline-block relative z-10 group-hover:scale-110`}
+                          src={`/skill/${skill.id}.png`}
+                          alt="skill"
+                          width={40}
+                          height={40}
+                          priority
+                          draggable={false}
+                          onClick={() => updateSkillLevel(skill.id, 20)}
+                        />
+                        <span
+                          className={`${commonStyle} inline-block absolute z-20 -right-6 lg:-right-3 text-sm -translate-x-10 translate-y-5 group-hover:scale-110 drop-shadow-[1px_1px_3px_#FF0000]`}
+                        >
+                          {textSkillLevel(skill.level)}
+                        </span>{" "}
+                        <span
+                          className={`${commonStyle} inline-block -translate-x-8 group-hover:translate-x-0 hover:scale-125 text-red-400`}
+                          onClick={() => updateSkillLevel(skill.id, -1)}
+                        >
+                          -
+                        </span>
+                      </h2>
+                    </span>
+                  ) : (
+                    <span key={dataIndex} />
+                  )
+              )}
+            </div>
           )
         )}
       </div>
