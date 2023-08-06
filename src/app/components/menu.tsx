840754b0c63eb699c8025e86d83f0e2ac2d5bbb0
taewaponb <taewapon.b@gmail.com>
2023-08-06 23:15:35 +0700

    :mag: Change from link to button

--- src/app/components/menu.tsx
+++ src/app/components/menu.tsx
@@ -63,14 +63,14 @@ export const Menu = () => {
     function?: () => void;
   }) => {
     return (
-      <a
+      <button
         key={data.header}
         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         onClick={data.function}
       >
         <p className={`m-0 opacity-50`}>{data.header}</p>
         <h2 className={`mb-3 text-2xl font-semibold`}>{data.detail}</h2>
-      </a>
+      </button>
     );
   };
 
